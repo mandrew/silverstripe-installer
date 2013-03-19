865486efa87fbaa881542c51e4942357abc89372
Michael Andrewartha <mandrewnz@gmail.com>
2013-03-19 12:43:26 +0000

    Update _config.php
    Added +[user] to indicate different user emails, added instructions, set gd quality and set local to en_GB, added optional full text search include

--- mysite/_config.php
+++ mysite/_config.php
@@ -15,13 +15,13 @@ if (!defined('SS_ENVIRONMENT_FILE')) {
 		"password" => "",
 		"database" => ""
 	);
-	$emailWriter = new SS_LogEmailWriter('betterbrief@gmail.com');
+	$emailWriter = new SS_LogEmailWriter('betterbrief+[user]@gmail.com');
 	$emailWriter->setFormatter(new BB_LogErrorEmailFormatter());
 	SS_Log::add_writer($emailWriter);
 	Config::inst()->update('GoogleSitemap', 'google_notification_enabled', true);
 }
 
-Email::setAdminEmail('Better Brief <betterbrief@gmail.com>');
+Email::setAdminEmail('Better Brief <betterbrief+[user]@gmail.com>');
 
 // stop the user being able to select h1 in the editor!
 HtmlEditorConfig::get('cms')->setOption('theme_advanced_blockformats', 'p,h2,h3,h4,h5,h6,address,pre');
@@ -29,11 +29,22 @@ HtmlEditorConfig::get('cms')->setOption('theme_advanced_blockformats', 'p,h2,h3,
 //set admin user var for analytics for admin users
 LeftAndMain::require_javascript('mysite/javascript/admin-analytics.js');
 
+// Set mysql connections to use utf8 encoding
 MySQLDatabase::set_connection_charset('utf8');
 
 // Set the current theme. More themes can be downloaded from
 // http://www.silverstripe.org/themes/
 SSViewer::set_theme('default');
 
+// Set image quality
+GD::set_default_quality(85);
+
+// Set the correct default language, this is used for Users
+// and for the content-language meta tag
+i18n::set_locale('en_GB');
+
 // Enable nested URLs for this site (e.g. page/sub-page/)
 if(class_exists('SiteTree')) SiteTree::enable_nested_urls();
+
+//allow full search of the site
+//FulltextSearchable::enable();
